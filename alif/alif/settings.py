40236edbbe3196f5cb44c22fca2e763bb91b3600
Muaaweyahkp <muaaweyahkp@gmail.com>
2026-07-17 00:34:32 +0530

    removed whitenoise

--- alif/alif/settings.py
+++ alif/alif/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -143,12 +141,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
 
 # -----------------------------------------------------------------------------
